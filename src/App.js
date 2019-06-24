0f6ebb66b173c71311191e0e49957e3e0ca0f06e
mathwell <eageeva@hotmail.com>
2019-06-23 17:06:02 -0700

    navbar layout setup and style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Link } from "react-router-dom";
+import { Image,Nav, NavDropdown,Navbar, NavItem, FormGroup, FormControl, Button, Grid, Row, Thumbnail} from "react-bootstrap";
+//import Routes from './components/routes/Routes';
+import { connect } from 'react-redux';
+import logo from './images/Commonwealth_Logo.svg'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  var icon = (
+    <span className="logo">
+      <a href="/">
+        <img src={logo} alt="logo" /></a>
+    </span>
   );
-}
+  return (
+    <div className="container-fluid">      
+      <Navbar staticTop={true} fluid={true} brand={icon} toggleButton  collapseOnSelect expand="lg" bg="white" variant="primary" toggleNavKey={1}>
+      <Navbar.Brand href="#home">{icon}</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="mr-auto">      
+    </Nav>
+    <Nav>
+      <Nav.Link href="#speakers">Featured Speakers</Nav.Link>
+      <Nav.Link href="#agenda">Conference Sessions</Nav.Link>
+      <Nav.Link href="#highlights">Program Highlights</Nav.Link>
+      <Nav.Link href="#plan">Planning Your Trip</Nav.Link>
+      <Nav.Link href="#register">Register Now</Nav.Link>
+      <Nav.Link href="#faq">FAQs</Nav.Link>
+      <Nav.Link href="#contact" data-toggle="modal">Contact Us</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+      </div>
+    );
+  }
+
+
+  
 
 export default App;
